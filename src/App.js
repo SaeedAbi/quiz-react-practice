7c869d246209623e7667147271d4d3c115591f2d
SaeedAbi <abdary.saeed@gmail.com>
2024-05-22 16:22:47 +0330

    :tada: init react quiz project with components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import DateCounter from "./DateCounter";
+import Index from "./Components/Header";
+import Main from "./Components/Main";
 
 export default function App(){
-  return <div>
-    <DateCounter/>
+  return <div className='app'>
+    <Index/>
+    <Main className='main'>
+      <p>1.15</p>
+      <p>Question?</p>
+    </Main>
   </div>
 }
---
+++ src/Components/Header/index.js
@@ -0,0 +1,10 @@
+function Index() {
+  return (
+    <header className='app-header'>
+      <img src='../../../public/download.png' alt='React logo' />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Index;
